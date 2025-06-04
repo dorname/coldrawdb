40e53a679801628ad357b859ea9bdb48c29fa429
dorname <lgqfighting@163.com>
2025-06-04 22:01:05 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    // commonjs({
-    //   include: [/jsonc-parser/, /node_modules/],
-    //   transformMixedEsModules: true
-    // })
+    react()
   ],
   // 防止 Vite 处理 Tauri 的 API 导入
   optimizeDeps: {
